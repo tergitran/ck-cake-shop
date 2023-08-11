21ebf5411ae6414eb986a5dc47e5d76f342cef10
Tran Dan <tergitran@gmail.com>
2023-08-11 17:27:45 +0700

    implement skeleton loading

--- src/views/products/index.vue
+++ src/views/products/index.vue
@@ -1,9 +1,32 @@
 <template>
   <div class="products-wrapper">
-    <div class="products-wrapper__title-main"><b></b><span>Our products</span><b></b></div>
-    <div class="products-container">
-      <Product v-for="product in products" :product="product" :key="product.id"></Product>
+    <div class="products-wrapper__title-main">
+      <b></b><span>Our products</span><b></b>
     </div>
+    <b-skeleton-wrapper :loading="loading">
+      <template #loading>
+        <div class="products-container">
+          <div class="cart-container" v-for="index in 3" :key="index">
+            <div class="pb-4">
+              <b-skeleton-img aspect="1:1" width="100%"></b-skeleton-img>
+            </div>
+            <b-skeleton width="60%"></b-skeleton>
+            <b-skeleton width="100%"></b-skeleton>
+            <b-skeleton width="100%"></b-skeleton>
+            <b-skeleton width="10%"></b-skeleton>
+          </div>
+        </div>
+      </template>
+
+      <div class="products-container">
+        <Product
+          v-for="product in products"
+          :product="product"
+          :key="product.id"
+          :loading="loading"
+        ></Product>
+      </div>
+    </b-skeleton-wrapper>
   </div>
 </template>
 
--- src/views/products/script.js
+++ src/views/products/script.js
@@ -8,21 +8,29 @@ export default {
   data() {
     return {
       products: [],
+      loading: false,
     };
   },
   computed: {},
   created() {
+    this.loading = true;
     fetch("products.json")
       .then((res) => res.json())
       .then((data) => {
-        this.products = data.map((item) => {
-          return {
-            ...item,
-            price: item.is_on_discount
-              ? item.discounted_price
-              : item.original_price,
-          };
-        });
+        // filter available cakes
+        this.products = data
+          .filter((cake) => cake.stock_count > 0)
+          .map((item) => {
+            return {
+              ...item,
+              price: item.is_on_discount
+                ? item.discounted_price
+                : item.original_price,
+            };
+          });
+      })
+      .finally(() => {
+        this.loading = false;
       });
   },
   methods: {},
